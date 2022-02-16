95333c5c87a45a368ee08901353daab4ac50d962
kraevI <kraev.is@phystech.edu>
2022-02-16 14:56:37 +0300

    Добавлен №3. Второй всё ещё не работает.
    Третий номер реализован через введение переменной, по-другому не получается.

--- project2/project2/project2.cpp
+++ project2/project2/project2.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <cmath>
 
 bool is_a_prime_number(int x) {
 	bool R = 1;
@@ -46,14 +47,31 @@ void checking_the_set_for_prime_and_palindrome(int n) {
 	return R;
 }*/
 
-/*void coup_helper(int &x) {
-    
+/*int upper_bound(int &x) {
+	int i = 1;
+	while (i > 0) {
+		if (x / pow(10, i) == 0) return i;
+		i++;
+	}
 }*/
+
+void coup(int &x) {
+	int a = 0;
+	while (x > 0) {
+		a = 10 * a + x % 10;
+		x = x / 10;
+	}
+	x = a;
+}
 
 int main(){
 	int n;
 	std::cin >> n;
-    checking_the_set_for_prime_and_palindrome(n);
+
+    //checking_the_set_for_prime_and_palindrome(n);
+
 	//std::cout << is_the_sequence_correct(n);
-	//coup(n);
+
+	/*coup(n);
+	std::cout << n;*/
 }
