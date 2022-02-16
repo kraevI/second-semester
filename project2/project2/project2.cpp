1659fb62aea5ed0ad7192f26183bc1c0b88997fa
kraevI <kraev.is@phystech.edu>
2022-02-16 13:07:04 +0300

    Файл, с работающим номером 1, заготовками для 2 и 3.

---
+++ project2/project2/project2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+bool is_a_prime_number(int x) {
+	bool R = 1;
+	for (int i = 2; i <= x / 2; i++) {
+		if (x % i == 0) {
+			R = 0;
+			break;
+		}
+	}
+	return R;
+}
+
+bool is_a_palindrome(int &x) {
+	int a;
+	int y = x * 10;
+	a = x % 10;
+	while (x != 0) {
+		x = x / 10;
+		a = a * 10 + x % 10;
+	}
+	return y == a;
+}
+
+void checking_the_set_for_prime_and_palindrome(int n) {
+	for (int i = 1; i <= n; i++) {
+		int ai;
+		using std::cout;
+		std::cin >> ai;
+		cout << is_a_prime_number(ai) << "\n";
+		cout << is_a_palindrome(ai) << "\n";
+	}
+}
+
+/*bool is_the_sequence_correct(int n) {
+	char a, char b, bool R = 1;
+	std::cin >> a;
+	for (int i = 1; i <= n - 1; i++) {
+		std::cin >> b;
+		if (a == b) {
+			R = 0;
+			break;
+		}
+		a = b;
+	}
+	return R;
+}*/
+
+/*void coup_helper(int &x) {
+    
+}*/
+
+int main(){
+	int n;
+	std::cin >> n;
+    checking_the_set_for_prime_and_palindrome(n);
+	//std::cout << is_the_sequence_correct(n);
+	//coup(n);
+}
